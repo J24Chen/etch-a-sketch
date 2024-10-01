b2fa7c7f28ba04640acefd8b17bd002fb3f83e9c
J24Chen <Jchen2408@gmail.com>
2024-10-01 08:30:34 -0400

    add opacity, dynamic grid size

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const button = document.querySelector(".create-grid");
+const input = document.querySelector("#grid-size");
 
 let gridSize = 16;
+input.value = gridSize;
+
 
 function generateRandomColor() {
     let maxColorVal = 0xFFFFFF;
@@ -15,6 +18,8 @@ function generateRandomColor() {
 
 function newGrid() {
     //clear entire contents inside container first
+    (input.value <= 100 && input.value >= 1) ? gridSize = input.value : alert("Invalid number!")
+
     container.innerHTML = "";
     for(let i = 0; i < gridSize; i++){
         let row = document.createElement("div");
@@ -27,15 +32,25 @@ function newGrid() {
 
 
             column.addEventListener("mouseenter", () =>{
-                let color = generateRandomColor();
-                column.style.backgroundColor = color;
-                console.log(color);
+                if (column.classList.contains('colored')) {
+                    column.style.opacity = parseFloat(column.style.opacity) + 0.1;
+                } else { 
+                    let color = generateRandomColor();
+                    column.style.backgroundColor = color;
+                    column.style.opacity = 0.1;
+                    column.classList.add('colored')
+                }
             });
         }
     }
 }
 
-
+//On Enter keypress trigger same event as button 
+input.addEventListener("keypress", (e) =>{
+    if (e.key === "Enter") {
+        button.click();
+    }
+});
 
 
 newGrid();
